556c136ebfa10977409e786d045f250b2daf2fc4
HenriquePinheiro <pinheirohsp@gmail.com>
2022-01-21 13:58:02 -0300

    added removeDuplicate exercise

--- script.js
+++ script.js
@@ -1,28 +1,28 @@
-// Find most frequent item in an array
-const mostFrequent = arr => {
-    const obj = arr.reduce((acc, val) => {
-        if(val in acc)
-            acc[val]++
-        else   
-            acc[val] = 1
-        return acc
-    }, {})
-    let higher = -Infinity;
-    const targetKey = 
-    Object.keys(obj).reduce((acc, val) => {
-        if(obj[val] > higher){
-            higher = obj[val]
-            return val
-        } else return acc
-        
-    }, 0);
-    return `${targetKey} (${obj[targetKey]} times)`
+const removeDuplicate = arr =>{
+    arr.forEach((val, index) => {
+        arr.splice(index, 1)
+        // removes the item and, if there is still any equals, removes it; if not gives back the item
+        if(arr.includes(val))   
+            removeItems(arr, val)
+        else
+            arr.splice(index, 0, val)
+    })    
+    return arr
 }
 
-const array1 = [54, 56, 56]
-const array2 = ['a', 12, true, 'hello', false, 'hello']
-const array3 = [true, true, [54], 'MyLove', 'MyLove', 'Ana', 'MyLove']
+const removeItems = (array, value) => {
+    const index = array.indexOf(value)
+    array.splice(index, 1)
+    if(array.includes(value))
+        removeItems(array, value)
+}
 
-console.log(mostFrequent(array1))
-console.log(mostFrequent(array2))
-console.log(mostFrequent(array3))
+const array1 = [5, 12, 6, 6, 5]
+const array2 = ['Henrique', false, 'henrique', 6, false]
+const array3 = [5, 'Ana', 6, 'She', 'Hello']
+
+
+
+console.log(removeDuplicate(array1))
+console.log(removeDuplicate(array2))
+console.log(removeDuplicate(array3))
