8848f8d626d5468fc613ad2a720814deb8b3e50d
HenriquePinheiro <pinheirohsp@gmail.com>
2022-02-17 19:25:39 -0300

    styles done
    lacks logic

---
+++ script.js
@@ -0,0 +1,19 @@
+const $addBtn = document.querySelector('.add-btn')
+const $listContainer = document.querySelector('ul.todo-list')
+// <li class="todo-element">
+//     <div>
+//      <input type="radio">
+//      <span>Study JavaScript</span>
+//     </div>
+//     <i data-feather="trash-2"></i>
+// </li>
+
+
+
+
+// LOGIC
+
+/*
+When click
+
+*/
